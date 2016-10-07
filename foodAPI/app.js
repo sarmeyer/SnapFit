03c4badd784323ffa8f51bd1ccf23343ad2d6aa0
Sarah <sarahmichellemeyer@gmail.com>
2016-10-07 16:21:18 -0600

    working upload to s3, file not opening

--- foodAPI/app.js
+++ foodAPI/app.js
@@ -24,6 +24,12 @@ app.use('/', routes);
 app.use('/users', users);
 
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
--- foodAPI/routes/index.js
+++ foodAPI/routes/index.js
@@ -1,31 +1,42 @@
   var express = require('express');
   var router = express.Router();
-  var aws = require('aws-sdk');
-  var config = require('../config.js')
+  var config = require('../config.js');
   var crypto = require('crypto');
+  var AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: config.ACCESS_KEY, secretAccessKey: config.SECRET_KEY, region: 'us-west-2'
+});
 
 router.get('/', function(req,res,next){
 res.send('you are at the home page');
 });
-function sign(req, res, next) {
 
-    var fileName = req.body.fileName,
-        expiration = new Date(new Date().getTime() + 1000 * 60 * 5).toISOString();
-    var secret = 'sijdfnkf3';
-    var policy =
-    { "expiration": expiration,
-        "conditions": [
-            {"bucket": config.S3_BUCKET},
-            {"key": fileName},
-            {"acl": 'public-read'},
-            ["starts-with", "$Content-Type", ""],
-            ["content-length-range", 0, 524288000]
-        ]};
-
-    policyBase64 = new Buffer(JSON.stringify(policy), 'utf8').toString('base64');
-    signature = crypto.createHmac('sha1', secret).update(policyBase64).digest('base64');
-    res.json({bucket: config.S3_BUCKET, awsKey: config.ACCESS_KEY, policy: policyBase64, signature: signature});
+function S3upload(req, res, next){
+    var s3bucket = new AWS.S3({params: {Bucket: 'snapfitupload'}});
+    var s3Params = {
+            Bucket: s3bucket,
+            Key: req.body.fileName,
+            Expires: 60,
+            ContentType: fileType,
+            ACL: 'public-read'
+        };
+    s3bucket.createBucket(function() {
+        console.log('*********REQ******');
+        console.log(req.body);
+        var params = {Key: req.body.fileName, Body: 'snapFit Photo'};
+        s3.getSignedUrl('getObject', s3Params, function (err, url) {
+            console.log("The URL is", url);
+            });
+        s3bucket.upload(params, function(err, data) {
+            if (err) {
+            console.log("Error uploading data: ", err);
+            } else {
+            console.log("Successfully uploaded data to S3");
+            }
+        });
+    });
 }
- router.post('/signing', sign);
+ router.post('/upload', S3upload);
 
   module.exports = router;

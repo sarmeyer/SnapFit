be0d001f9227f7a79c485e1c297ea69aad5220ab
Sarah <sarahmichellemeyer@gmail.com>
2016-10-08 21:09:21 -0600

    updated node version

--- snapfit/www/js/app.js
+++ snapfit/www/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ionic', 'ngCordova', 'ngResource', 'ngFileUpload'])
+var app = angular.module('fileUpload', ['ionic', 'ngCordova', 'ngResource', 'ngFileUpload'])
     .value('nutritionix', {
         'appId': '67ca9680',
         'appKey': 'f2c1ffde813cfe43dad5a60f4f6faa15'
--- snapfit/www/js/controllers.js
+++ snapfit/www/js/controllers.js
@@ -1,61 +1,77 @@
-app.controller('main', function($scope, $http, DataService, DataServiceHTTP) {
+app.controller('imageCtrl', function ($scope, $http, Upload, $timeout) {
+    // ['$scope', '$http', 'Upload', '$timeout',
+    $scope.uploadPic = function(file) {
+        console.log('********ANGULAR UPLOAD PIC FUNCTION*****');
 
-$scope.upload = (function(){
-    $scope.uploadFiles = function(file, errFiles) {
-        $scope.f = file;
-        $scope.errFile = errFiles && errFiles[0];
-        if (file) {
-            file.upload = Upload.upload({
-                url: 'http://localhost:3030/upload',
-                data: {file: file}
-            });
+        var filename = file.name;
+        var type = file.type;
+        var query = {
+            filename: filename,
+            type: type,
+            crossDomain: true
+        };
 
-            file.upload.then(function (response) {
-                $timeout(function () {
-                    file.result = response.data;
+        $http.post('http://localhost:3030/signing', query)
+            .success(function(result) {
+                console.log('*********SIGNING RESULT********');
+                console.log(result);
+                Upload.upload({
+                    url: 'https://snapfitupload.s3.amazonaws.com/', //S3 upload url including bucket name
+                    transformRequest: function(data, headersGetter) {
+                        var headers = headersGetter();
+                        delete headers.Authorization;
+                        return data;
+                    },
+                    fields: result.fields, //credentials
+                    method: 'POST',
+                    file: file
+                }).progress(function(evt) {
+                    console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
+                }).error(function() {
+
                 });
-            }, function (response) {
-                if (response.status > 0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function (evt) {
-                file.progress = Math.min(100, parseInt(100.0 *
-                                         evt.loaded / evt.total));
-            });
-        }
-    }
-}());
-
-
-    $scope.getAccess = function(){
-        var tokenData = imageService.getToken();
-        tokenData.then(function(response){
-            var token = response.data.access_token;
-        var imgData = imageService.getTags({
-            'token': token,
-            'imageUrl': srcImage.getAttribute('src')
+            })
+            .error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
         });
-            imgData.then(function(data){
-                $scope.ingredients = data.data.results[0].result.tag.classes;
-                $scope.data.searchKey = $scope.ingredients[0];
-                $scope.doSearch();
-            });
-        });
-    };
-
-    $scope.data = { searchKey: '' };
-
-    $scope.doSearch = function() {
-        if ( true ) {
-        var promise = DataService.getAll( {
-            'term' : $scope.data.searchKey,
-            'results':'0:50',
-        }).$promise;
-        promise.then(function(_response) {
-            $scope.items = _response.hits;
-        });
-        }
     };
 });
+
+
+//     $scope.getAccess = function(){
+//         var tokenData = imageService.getToken();
+//         tokenData.then(function(response){
+//             var token = response.data.access_token;
+//         var imgData = imageService.getTags({
+//             'token': token,
+//             'imageUrl': srcImage.getAttribute('src')
+//         });
+//             imgData.then(function(data){
+//                 $scope.ingredients = data.data.results[0].result.tag.classes;
+//                 $scope.data.searchKey = $scope.ingredients[0];
+//                 $scope.doSearch();
+//             });
+//         });
+//     };
+
+//     $scope.data = { searchKey: '' };
+
+//     $scope.doSearch = function() {
+//         if ( true ) {
+//         var promise = DataService.getAll( {
+//             'term' : $scope.data.searchKey,
+//             'results':'0:50',
+//         }).$promise;
+//         promise.then(function(_response) {
+//             $scope.items = _response.hits;
+//         });
+//         }
+//     };
+// });
 
     // $scope.takeImage = function() {
     //      var options = {

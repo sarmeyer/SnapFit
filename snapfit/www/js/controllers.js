c0a909deca67e730c79236bcf7e018e0e81fd4ea
Sarah <sarahmichellemeyer@gmail.com>
2016-10-10 14:53:55 -0600

    camera working and image displaying

--- snapfit/www/js/controllers.js
+++ snapfit/www/js/controllers.js
@@ -1,99 +1,96 @@
-app.controller('imageCtrl', function ($scope, imageService, DataService, $cordovaCamera) {
-    $scope.getAccess = function(){
+app.controller('imageCtrl', function($scope, $ionicActionSheet, $cordovaCamera, APIservice, DataService) {
+
+  $scope.addMedia = function() {
+    $scope.hideSheet = $ionicActionSheet.show({
+      buttons: [{
+        text: 'Take photo'
+      }, {
+        text: 'Photo from library'
+      }],
+      titleText: 'Add images',
+      cancelText: 'Cancel',
+      buttonClicked: function(index) {
+        $scope.takePhoto(index);
+        return true;
+      }
+    });
+  }
+  $scope.takePhoto = function(type) {
+    var source;
+    switch (type) {
+      case 0:
+        source = Camera.PictureSourceType.CAMERA;
+        break;
+      case 1:
+        source = Camera.PictureSourceType.PHOTOLIBRARY;
+        break;
+    }
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: source,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.imgURI = imageData;
+    }, function(err) {});
+  }
+
+  $scope.foodAPI = function(foodImages) {
+    var tokenData = APIservice.getToken();
+    tokenData.then(function(response) {
+      var token = response.data.access_token;
+      var imgData = APIservice.getTags({
+        'token': token,
+        'imageUrl': encodeURI(foodImages)
+      });
+      imgData.then(function(data) {
+        $scope.ingredients = data.data.results[0].result.tag.classes;
+        $scope.data.searchKey = $scope.ingredients[0];
+        $scope.doSearch();
+      });
+    });
+  };
+
+  $scope.getAccess = function() {
     var image = angular.element('#url').attr('src');
     var imageURI = encodeURI(image);
 
-        var tokenData = imageService.getToken();
-            tokenData.then(function(response){
-                var token = response.data.access_token;
-                var imgData = imageService.getTags({
-                    'token': token,
-                    'imageUrl': imageURI
-                });
-                    imgData.then(function(data){
-                        $scope.ingredients = data.data.results[0].result.tag.classes;
-                        $scope.data.searchKey = $scope.ingredients[0];
-                        $scope.doSearch();
-                    });
-                });
-            };
+    var tokenData = APIservice.getToken();
+    tokenData.then(function(response) {
+      var token = response.data.access_token;
+      var imgData = APIservice.getTags({
+        'token': token,
+        'imageUrl': imageURI
+      });
+      imgData.then(function(data) {
+        $scope.ingredients = data.data.results[0].result.tag.classes;
+        $scope.data.searchKey = $scope.ingredients[0];
+        $scope.doSearch();
+      });
+    });
+  };
 
-    $scope.data = { searchKey: '' };
+  $scope.data = {
+    searchKey: ''
+  };
 
-    $scope.doSearch = function() {
-        if ( true ) {
-        var promise = DataService.getAll( {
-            'term' : $scope.data.searchKey,
-            'results':'0:50',
-        }).$promise;
-        promise.then(function(_response) {
-            $scope.items = _response.hits;
-        });
-        }
-    };
+  $scope.doSearch = function() {
+    if (true) {
+      var promise = DataService.getAll({
+        'term': $scope.data.searchKey,
+        'results': '0:50',
+      }).$promise;
+      promise.then(function(_response) {
+        $scope.items = _response.hits;
+      });
+    }
+  };
 
- $scope.images = [];
-
-    $scope.addImage = function() {
-        var options = {
-		destinationType : Camera.DestinationType.FILE_URI,
-		sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-		allowEdit : false,
-		encodingType: Camera.EncodingType.JPEG,
-		popoverOptions: CameraPopoverOptions,
-	};
-    	$cordovaCamera.getPicture(options).then(function(imageData) {
-            onImageSuccess(imageData);
-		function onImageSuccess(fileURI) {
-			createFileEntry(fileURI);
-            $scope.imageData = fileURI;
-		}
-        function createFileEntry(fileURI) {
-			window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-		}
-        function copyFile(fileEntry) {
-                    var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-                    var newName = makeid() + name;
-
-                    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-                        fileEntry.copyTo(
-                            fileSystem2,
-                            newName,
-                            onCopySuccess,
-                            fail
-                        );
-                    },
-                    fail);
-                }
-        function onCopySuccess(entry) {
-            $scope.$apply(function () {
-                $scope.images.push(entry.nativeURL);
-            });
-        }
-        function fail(error) {
-			console.log("fail: " + error.code);
-		}
-
-		function makeid() {
-			var text = "";
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-			for (var i=0; i < 5; i++) {
-				text += possible.charAt(Math.floor(Math.random() * possible.length));
-			}
-			return text;
-		}
-        }, function(err) {
-            console.log(err);
-        });
-        console.log("add image");
-    };
-
-    $scope.urlForImage = function(imageName) {
-        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-        var trueOrigin = cordova.file.dataDirectory + name;
-        return trueOrigin;
-    };
-
-    $scope.formData = {};
-});     
+  $scope.formData = {};
+});
--- snapfit/www/js/services.js
+++ snapfit/www/js/services.js
@@ -1,74 +1,53 @@
-app.factory('imageService', function($http){
-    return {
-        getToken: function(){
-            return $http.post('https://api.clarifai.com/v1/token/?client_id=Fi1oy98C5k1GWRFjZHDBKb0OVPAefQh5anrvnVoB&client_secret=sH7N9LUeacLgmdUlMPKQWTH3zdhUC8NwKRL3bs-t&grant_type=client_credentials');
-        },
-        getTags: function(accessData){
-            return $http.get('https://api.clarifai.com/v1/tag/?model=food-items-v1.0&url='+ accessData.imageUrl, {
-                    headers: {'Authorization': 'Bearer ' + accessData.token}
-            });
-        }
-    };
-});
-app.factory('FileService', function() {
-  var images;
-  var IMAGE_STORAGE_KEY = 'images';
-
-  function getImages() {
-    var img = window.localStorage.getItem(IMAGE_STORAGE_KEY);
-    if (img) {
-      images = JSON.parse(img);
-    } else {
-      images = [];
-    }
-    return images;
-  };
-
-  function addImage(img) {
-    images.push(img);
-    window.localStorage.setItem(IMAGE_STORAGE_KEY, JSON.stringify(images));
-  };
-
+app.factory('APIservice', function($http) {
   return {
-    storeImage: addImage,
-    images: getImages
-  }
-})
-
+    getToken: function() {
+      return $http.post('https://api.clarifai.com/v1/token/?client_id=Fi1oy98C5k1GWRFjZHDBKb0OVPAefQh5anrvnVoB&client_secret=sH7N9LUeacLgmdUlMPKQWTH3zdhUC8NwKRL3bs-t&grant_type=client_credentials');
+    },
+    getTags: function(accessData) {
+      return $http.get('https://api.clarifai.com/v1/tag/?model=food-items-v1.0&url=' + accessData.imageUrl, {
+        headers: {
+          'Authorization': 'Bearer ' + accessData.token
+        }
+      });
+    }
+  };
+});
 app.factory('DataService', function($resource, nutritionix) {
-    var aSearchObject = $resource('https://api.nutritionix.com/v1_1/search/:term', { term: '@term' }, {
-        getAll: {
-            method: 'get',
-            params: {
-                results: ':results',
-                appId: nutritionix.appId,
-                appKey: nutritionix.appKey,
-                fields: ':fields',
-            }
-        }
-    });
-    return {
-        getAll: function(_params) {
-            var defaultFields = 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat';
-            if (!_params.fields) {
-                _params.fields = defaultFields;
-            }
-            return aSearchObject.getAll(_params);
-        }
-    };
+  var aSearchObject = $resource('https://api.nutritionix.com/v1_1/search/:term', {
+    term: '@term'
+  }, {
+    getAll: {
+      method: 'get',
+      params: {
+        results: ':results',
+        appId: nutritionix.appId,
+        appKey: nutritionix.appKey,
+        fields: ':fields',
+      }
+    }
+  });
+  return {
+    getAll: function(_params) {
+      var defaultFields = 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat';
+      if (!_params.fields) {
+        _params.fields = defaultFields;
+      }
+      return aSearchObject.getAll(_params);
+    }
+  };
 });
 
 app.factory('DataServiceHTTP', function($http, nutritionix) {
-    return {
-        getAll: function(_key) {
-            return $http.get('https://api.nutritionix.com/v1_1/search/' + _key, {
-                'params': {
-                    results: '0:50',
-                    appId: nutritionix.appId,
-                    appKey: nutritionix.appKey,
-                    fields: 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat'
-                }
-            });
+  return {
+    getAll: function(_key) {
+      return $http.get('https://api.nutritionix.com/v1_1/search/' + _key, {
+        'params': {
+          results: '0:50',
+          appId: nutritionix.appId,
+          appKey: nutritionix.appKey,
+          fields: 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat'
         }
-    };
+      });
+    }
+  };
 });
